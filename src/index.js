600428b234ca0433f0ca36014999642b4374fbbe
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-05-14 00:00:34 +0100

    index.js: Created consts for errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import "./style.css";
 const form = document.querySelector("form");
 const email = document.getElementById("mail");
-const country = document.getElementById("postcode");
-const postcode = document.getElementById("mail");
+const emailError = document.querySelector("#mail + span.error")
+const country = document.getElementById("country");
+const countryError = document.querySelector("#country + span.error");
+const postcode = document.getElementById("postcode");
+const postcodeError = document.querySelector("#postcode + span.error");
 const password = document.getElementById("password");
+const passwordError = document.querySelector("#password + span.error")
 const passwordConfirm = document.getElementById("passwordConfirm");
+const passwordConfirmError = document.querySelector("#passwordConfirm + span.error");
