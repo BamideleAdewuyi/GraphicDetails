c5d239a04789e61458e7d5d74c071c0c89f7f5c3
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-05-13 23:30:56 +0100

    index.js: Created consts for all form inputs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import "./style.css";
 const form = document.querySelector("form");
 const email = document.getElementById("mail");
+const country = document.getElementById("postcode");
+const postcode = document.getElementById("mail");
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("passwordConfirm");
