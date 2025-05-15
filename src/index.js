a2f2a0e7e54626382128316da9055e3142c8d6ce
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-05-16 00:01:33 +0100

    index.js: Created checkPostcode function

--- src/index.js
+++ src/index.js
@@ -10,3 +10,15 @@ const password = document.getElementById("password");
 const passwordError = document.querySelector("#password + span.error")
 const passwordConfirm = document.getElementById("passwordConfirm");
 const passwordConfirmError = document.querySelector("#passwordConfirm + span.error");
+
+//Use RegEx for postcode and passwords
+// Postcode regex: ([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})
+// All: Value missing
+// email: validity valid
+// postcode: does not match format
+// password, email: too short
+// password confirm: does not match
+
+function checkPostcode() {
+    
+}
